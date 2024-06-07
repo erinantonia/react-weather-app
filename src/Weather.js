aac097b9239ea9cc20ea75a8dfd2a610b80b981e
erinantonia <145416233+erinantonia@users.noreply.github.com>
2024-06-07 12:18:09 +1200

    Changed Weather.js content

--- src/Weather.js
+++ src/Weather.js
@@ -1,77 +1,82 @@
-import React from "react";
-
-// footer links, how to make it look right without being functional?
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Weather() {
+  const [city, setCity] = useState("");
+  const [message, setMessage] = useState("");
+  const [temperature, setTemperature] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [humidity, setHumidity] = useState(null);
+  const [windSpeed, setWindSpeed] = useState(null);
+  const [icon, setIcon] = useState(null);
+
+  function showWeather(response) {
+    setTemperature(response.data.main.temp);
+    setDescription(response.data.weather.description);
+    setHumidity(response.data.main.humidity);
+    setWindSpeed(response.data.wind.speed);
+    setIcon(response.data.weather.icon);
+  }
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a05960e74660eda28aaa507eb0f43896&units=metric
+  `;
+  axios.get(url).then(showWeather);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setMessage(
+      <ul>
+        <li>Temperature: {Math.round(temperature)}°C</li>
+        <li>Description: {description}</li>
+        <li>Humidity: {humidity}%</li>
+        <li>Wind speed: {Math.round(windSpeed * 3.6)}kph</li>
+        <li>{icon}</li>
+      </ul>
+    );
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
   return (
     <div className="Weather">
-      <body>
-        <div class="weather-app">
-          <header>
-            <form class="search-form" id="search-form">
-              <input
-                type="search"
-                placeholder="Enter a city..."
-                required
-                id="search-form-input"
-                class="search-form-input"
-              />
-              <input type="submit" value="Search" class="search-form-button" />
-            </form>
-          </header>
-
-          <main>
-            <div class="weather-app-data">
-              <div>
-                <h1 class="weather-app-city" id="city">
-                  Auckland
-                </h1>
-                <p class="weather-app-details">
-                  <span id="time"></span> <span id="description"></span>
-                  <br />
-                  Humidity: <strong id="humidity"></strong> Wind:
-                  <strong id="wind-speed"></strong>
-                </p>
-              </div>
-              <div class="weather-app-temperature-container">
-                <div id="icon"></div>
-                <div class="weather-app-temperature" id="temperature">
-                  17
-                </div>
-                <div class="weather-app-unit">°C</div>
-              </div>
-            </div>
-            <div class="weather-forecast" id="forecast"></div>
-          </main>
-
-          <footer>
-            This project was coded by
-            <a
-              href="https://github.com/erinantonia"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Erin Allen,
-            </a>
-            code hosted
-            <a
-              href="https://github.com/erinantonia/react-weather-app"
-              target="_blank"
-              rel="noreferrer"
-            >
-              on Github
-            </a>
-            and website hosted
-            <a
-              href="https://master--react-weather-app-ea.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              on Netlify
-            </a>
-          </footer>
-        </div>
-      </body>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Search a city..."
+          onChange={updateCity}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+      <p>{message}</p>
+      <footer>
+        This project was coded by
+        <a
+          href="https://github.com/erinantonia"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Erin Allen,
+        </a>
+        code hosted
+        <a
+          href="https://github.com/erinantonia/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          on Github
+        </a>
+        and website hosted
+        <a
+          href="https://master--react-weather-app-ea.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          on Netlify
+        </a>
+      </footer>
+      ;
     </div>
   );
 }
